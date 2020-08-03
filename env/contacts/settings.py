9dc7cb1943e182b8d21f46233e191dbd1801b01a
kline <klinetek@gmail.com>
2020-08-03 10:43:28 -0400

    setting up contact models

--- env/contacts/settings.py
+++ env/contacts/settings.py
@@ -119,3 +119,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+#Setting media staticfiles
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
